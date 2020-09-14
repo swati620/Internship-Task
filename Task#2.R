f433af254b4b45fd241175c17ab6ea9119682485
swati620 <71165247+swati620@users.noreply.github.com>
2020-09-14 13:57:38 +0530

    Update Task#2.R

--- Task#2.R
+++ Task#2.R
@@ -1,3 +1,6 @@
+#simple linear regression: In this regression task we will predict the percentage of
+#marks that a student is expected to score based upon the
+#number of hours they studied.
 library(readr)
 #Importing data through url
 
